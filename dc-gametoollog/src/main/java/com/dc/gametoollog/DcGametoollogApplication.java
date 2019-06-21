a6c4347f8ee1c556a8e1425d9e51dd324c281571
wangy <wangy@shanggame.com>
2019-06-21 18:44:11 +0800

    添加项目

---
+++ dc-gametoollog/src/main/java/com/dc/gametoollog/DcGametoollogApplication.java
@@ -0,0 +1,13 @@
+package com.dc.gametoollog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcGametoollogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcGametoollogApplication.class, args);
+	}
+
+}
---
+++ dc-gametoollog/src/test/java/com/dc/gametoollog/DcGametoollogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dc.gametoollog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DcGametoollogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
